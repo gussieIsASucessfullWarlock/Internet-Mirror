1a721c3e6b6ef7a0a8f785b3f80f887668292a17
E. S <82612866+gussieIsASucessfullWarlock@users.noreply.github.com>
2022-04-30 21:12:23 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,24 +14,24 @@ sudo systemctl start grafana-server
 sudo systemctl enable grafana-server.service
 apt install mysql-server -y
 clear
-mysql-secureinstallation
+mysql-secure-installation
 pip install mysql-connector-python
 chmod 777 test.py 
 cp test.py /bin/stest.py
 ufw allow 3006
 sudo systemctl restart mysql
 clear
-echo “Edit crontab to make this run at boot. With sudo crontab -e and the file location is: /bin/stest.py.”
+echo Edit crontab to make this run at boot. With sudo crontab -e and the file location is: /bin/stest.py.
 sleep 1
-echo “Rebooting in 5”
+echo Rebooting in 5
 sleep 1
-echo “Rebooting in 4”
+echo Rebooting in 4
 sleep 1
-echo “Rebooting in 3”
+echo Rebooting in 3
 sleep 1
-echo “Rebooting in 2”
+echo Rebooting in 2
 sleep 1
-echo “Rebooting in 1”
+echo Rebooting in 1
 sleep 1
 echo “Rebooting”
 reboot
